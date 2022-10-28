21525a04913c39840b1a5f9eedd2e92e7d0ee766
Annie Öhlén <annie.ohlen@outlook.com>
2022-10-28 23:46:04 +0200

    Last update.

--- Program.cs
+++ Program.cs
@@ -1,137 +1,136 @@
 ﻿
-//In a few places Y/N? have been used, meaning YES/NO?.
-//It has been shorted to make testing the program faster with just an y or n for the user.
+/*
+In a few places Y/N? have been used, meaning YES/NO?.
+It has been shorted to make testing the program faster with just an y or n for the user.
+*/
 
-//Color styling of the console.
+//Styling of the console:
 Console.BackgroundColor = ConsoleColor.DarkGray;
 Console.Clear();
 Console.ForegroundColor = ConsoleColor.Black;
+Console.WindowHeight = 40;
+Console.WindowWidth = 80;
 
-//Dictionary instead of list in order to connect two types.
+//Dictionary instead of list in order to connect two types:
 Dictionary<string, int> calculationsList = new Dictionary<string, int>(2);
 
+//Bool true for the program running:
 bool programRunning = true;
 while (programRunning)
-
 {
     //Welcoming to the calculator, some random stuff added for styling.
-    Console.WriteLine("\n< § ~ { The Basic Calculator } ~ § >\n");
+    Console.WriteLine("\n< § ~ { The Basic Calculator } ~ § >");
+    Console.WriteLine("\nI bid thee welcome to a simple calculator! \n");
 
-    //Meny choices.
-    Console.WriteLine("[1] < Calculation of two numbers >");
+    //Numbered meny:
+    Console.WriteLine("[1] < Calculation of two numbers (integers) >");
     Console.WriteLine("[2] < Show previous results >");
     Console.WriteLine("[3] < Clear the results >");
     Console.WriteLine("[4] < Exit the program >");
 
-    //Tryparse for the meny choice with the switch default to inform if not 1-3 have been entered.
+    //Tryparse for the above meny choice with the switch default to inform if not 1-4 have been entered:
     Int32.TryParse(Console.ReadLine(), out int menyChoice);
     switch (menyChoice)
     {
         case 1:
-            //Nedan är uträkningarna. Det finns garanterat ett bättre sätt att göra detta på då de fyra olika kodblocken är väldigt lika.
-            //Någon form av loop kanske? Men detta är den nivå jag kan förstå just nu.
+            /* Down below  in case 1 are the calculations, I believe there is a better way to do this as it is a repeatable code for the most part.
+            I wrote the following code as I thought it easy for me to understand if something went wrong, logics, and also easy to read.
+            */
             try
-            {
+            {//Used question mark for the string to avoid null warnings (catch at the end of try).
                 Console.WriteLine("\nPlease enter the first number: ");
                 string? firstNumberText = Console.ReadLine();
                 int firstNumber = Convert.ToInt32(firstNumberText);
 
+                //This was tricky with the operators. First I made it more difficult than neccesary (converting string to char) but realized it was not neccesary.
                 Console.WriteLine("\nChose an operator ( + - * / ): ");
-                string? operatorChoiceText = Console.ReadLine();
-                char operatorChoice = Convert.ToChar(operatorChoiceText); //Null solved with catch.
-
+                string? operatorChoice = Console.ReadLine();
 
                 Console.WriteLine("\nPlease enter the second number: ");
                 string? secondNumberText = Console.ReadLine();
                 int secondNumber = Convert.ToInt32(secondNumberText);
 
-                string userCalculationInput = $"{firstNumberText} {operatorChoiceText} {secondNumberText}";
+                //A string to gather the seperate inputs from the user for the one string in Dictionary:
+                string userCalculationInput = $"{firstNumberText} {operatorChoice} {secondNumberText}";
+                //An int for the result to add to the one int in Dictionary:
                 int calculatedResult = 0;
 
-                if (operatorChoice == '+')
+                /*
+                Several if statements for the for operators used.
+                Tried to make the code cleaner by having the calculation in the WriteLine to the Console.
+                */
+                if (operatorChoice == "+")
                     Console.WriteLine($"\nInput: {firstNumber} {operatorChoice} {secondNumber}. Result: {calculatedResult = firstNumber + secondNumber} ");
 
-                if (operatorChoice == '-')
+                if (operatorChoice == "-")
                     Console.WriteLine($"\nInput: {firstNumber} {operatorChoice} {secondNumber}. Result: {calculatedResult = firstNumber - secondNumber} ");
 
-                if (operatorChoice == '*')
+                if (operatorChoice == "*")
                     Console.WriteLine($"\nInput: {firstNumber} {operatorChoice} {secondNumber}. Result: {calculatedResult = firstNumber * secondNumber} ");
 
-                if (operatorChoice == '/')
+                if (operatorChoice == "/")
                     Console.WriteLine($"\nInput: {firstNumber} {operatorChoice} {secondNumber}. Result: {calculatedResult = firstNumber / secondNumber} ");
 
+                //The string and the int gets added to Dictionary.
                 calculationsList.Add(userCalculationInput, calculatedResult);
                 Console.ReadKey();
-                break;
-
             }
             catch
             {
+                //Catch for errors, incorrect inputs, also ask the user if they would like to retry.
                 Console.WriteLine($"Error: Incorrect input. \nRetry? Y/N?");
                 string? retryCase1 = Console.ReadLine();
-                if (retryCase1 == "y")
+                if (retryCase1?.ToLower() == "y")
                     goto case 1;
 
-                else if (retryCase1 != "y")
+                else if (retryCase1?.ToLower() != "y")
                             //You will be returned to the meny. WriteLine and then a 2 second delay for break? Possible?
                             break;
             }
             break;
 
         case 2:
+            //A somple case for showing Dictionary as input and result list to the user.
             Console.WriteLine("\nShow the previous results of the calculator? Y/N?");
             string? showPreviousResults = Console.ReadLine();
 
             if (showPreviousResults?.ToLower() == "y")
             {
-                Console.WriteLine("\nComplete list of the calculations: \n");
+                //The foreach could be changed out for a for loop but I found the foreach less cluttered in this case.
                 int calculationOrder = 1;
-                for (int i = 0; i < calculationsList.Count; i++)
-                {
-                    Console.WriteLine($"Calculation #{calculationOrder++}: " + calculationsList.ElementAt(i).Key + " = " + calculationsList.ElementAt(i).Value);
-                }
-
-
-                //Alternativt:
-                /*
                 foreach (var item in calculationsList)
                 {
-                    Console.WriteLine($"Calculation #{calculationsList.Count}: [" + item.Key + " = " + item.Value + "]");
+                    Console.WriteLine($"Calculation #{calculationOrder++}: " + item.Key + " = " + item.Value + "");
                 }
-                */
-
-
                 Console.ReadKey();
-                break;
             }
 
-            else if (showPreviousResults?.ToLower() != "y") //You will be returned to the meny. WriteLine and then a 2 second delay for break? Possible?
+            else if (showPreviousResults?.ToLower() != "y") //"You will be returned to the meny." WriteLine and then a 2 second delay for break? An idea.
                 break;
             break;
 
+            //I decided to add the possibilty for the user to clear Dictionary without restarting the program:
         case 3:
-            Console.WriteLine("If you clear the results you will not be able to get them back. Clear the results? Y/N?");
+            Console.WriteLine("\nIf you clear the results you will not be able to get them back. \nClear the results? Y/N?");
             string? clearResultsChoice = Console.ReadLine();
 
             if (clearResultsChoice?.ToLower() == "y")
+            {
                 calculationsList.Clear();
-
-            else if (clearResultsChoice?.ToLower() == "n")
-            {
-                Console.WriteLine("The results will not be cleared.");
+                Console.WriteLine("\nThe results have been cleared. \nPress a key to return to the meny.");
                 Console.ReadKey();
-                break;
             }
-            else
+
+            else if (clearResultsChoice?.ToLower() != "y")
             {
-                Console.WriteLine("Error: You did not enter Y or N.");
-                //Console.P();
-                goto case 3;
+                Console.WriteLine("\nThe results will not be cleared. \nPress a key to return to the meny.");
+                Console.ReadKey();
             }
             break;
 
+            //Option to exit the program while making sure that the user do not accidently exit:
         case 4:
-            Console.WriteLine("If you exit the program the calculations will not be saved. Exit the program? Y/N?");
+            Console.WriteLine("\nIf you exit the program the calculations will not be saved. \nExit the program? Y/N?");
             string? exitYesNo = Console.ReadLine();
 
             if (exitYesNo?.ToLower() == "y")
@@ -142,10 +141,10 @@ while (programRunning)
             break;
 
         default:
-            Console.WriteLine("You have to chose between meny choices 1 - 3! \nPress a key and then try again.");
+            Console.WriteLine("\nYou have to chose between meny choices 1 - 3! \nPress a key and then try again.");
+            Console.ReadKey();
             break;
     }
-
-    //To clear the console.
+    //To clear the console between uses.
     Console.Clear();
 }
--- README.md
+++ README.md
@@ -1,26 +1,2 @@
 # Basic Calculator
-
-            //Pseudo code in Swedish
-            /*
-            
-            DONE. Välkomnande meddelande
-            
-            DONE. En lista för att spara historik för räkningar
-
-            DONE. Användaren matar in tal och matematiska operation
-
-            -OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
-
-            DONE. Ifall användaren skulle dela 0 med 0 visa Ogiltig inmatning!
-
-            DONE. Lägga resultat till listan
-
-            DONE. Visa resultat
-
-            DONE. Fråga användaren om den vill visa tidigare resultat.
-
-            DONE. Visa tidigare resultat
-
-            DONE. Fråga användaren om den vill avsluta eller fortsätta.
-
-            */
+See comments in the code.
