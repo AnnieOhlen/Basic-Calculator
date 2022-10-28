20c4eaf3ab58dbe7b9c2684e58bcd1515b7c00d0
Annie Öhlén <annie.ohlen@outlook.com>
2022-10-28 07:15:08 +0200

    Update.

--- Program.cs
+++ Program.cs
@@ -1,38 +1,110 @@
-﻿namespace Basic_Calculator
+﻿
+
+
+//In a few places Y/N? have been used, meaning YES/NO?.
+//It has been shorted to make testing the program faster with just an y or n for the user.
+
+//Color styling of the console.
+Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Black;
+
+
+
+List<int> calculatedResultsList = new List<int>();
+int calculatedResult = 0;
+
+
+
+bool programRunning = true;
+while (programRunning)
+
 {
-    internal class Program
+    //To clear the console of the meny between meny choices.
+    //Console.Clear();
+
+    //Welcoming to the calculator, some random stuff added for styling.
+    Console.WriteLine("\n< § ~ { The Basic Calculator } ~ § >\n");
+
+    //Meny choices.
+    Console.WriteLine("[1] < Calculation of two numbers >");
+    Console.WriteLine("[2] < Show previous results >");
+    Console.WriteLine("[3] < Exit the program >");
+
+    //Tryparse for the meny choice with the switch default to inform if not 1-3 have been entered.
+    Int32.TryParse(Console.ReadLine(), out int menyChoice);
+    switch (menyChoice)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        case 1:
 
-            //Pseudo kod
-            // Välkomnande meddelande
-            // En lista för att spara historik för räkningar
-            // Användaren matar in tal och matematiska operation
-            //OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
-            // Ifall användaren skulle dela 0 med 0 visa Ogiltig inmatning!
-            // Lägga resultat till listan
-            //Visa resultat
-            //Fråga användaren om den vill visa tidigare resultat.
-            //Visa tidigare resultat
-            //Fråga användaren om den vill avsluta eller fortsätta.
+            //Nedan är uträkningarna. Det finns garanterat ett bättre sätt att göra detta på då de fyra olika kodblocken är väldigt lika.
+            //Någon form av loop kanske? Men detta är den nivå jag kan förstå just nu.
 
+            //calculatedResultsList.Add(calculatedResult);
+            try
+            {
+                Console.WriteLine("Please enter a number: ");
+                string? firstNumberText = Console.ReadLine();
+                int firstNumber = Convert.ToInt32(firstNumberText);
 
-            //Lista för att spara, t-list?
-
-            Console.WriteLine("Hello, World!");
-
-            Console.WriteLine(" — — — — — — — — — — — — — ");
-            Console.WriteLine("< § ~ { The Fun Calcylator } ~ § >");
-            Console.WriteLine("[1] ");
-            Console.WriteLine("[2] ");
-            Console.WriteLine("[3] ");
-            Console.WriteLine("[4] ");
-            Console.WriteLine("[5] ");
-            Console.WriteLine(" — — — — — — — — — — — — — ");
+                Console.WriteLine("Chose an operator ( + - * / ): ");
+                string? operatorChoiceText = Console.ReadLine();
+                char operatorChoice = Convert.ToChar(operatorChoiceText);
 
 
+                Console.WriteLine("Please enter a number: ");
+                string? secondNumberText = Console.ReadLine();
+                int secondNumber = Convert.ToInt32(secondNumberText);
+
+
+                if (operatorChoice == '+')
+                    Console.WriteLine($"Entererd: {firstNumber} {operatorChoice} {secondNumber}. Result: {calculatedResult = firstNumber + secondNumber} ");
+
+                if (operatorChoice == '-')
+                    Console.WriteLine($"Entererd: {firstNumber} {operatorChoice} {secondNumber}. Result: {calculatedResult = firstNumber - secondNumber} ");
+
+                if (operatorChoice == '*')
+                    Console.WriteLine($"Entererd: {firstNumber} {operatorChoice} {secondNumber}. Result: {calculatedResult = firstNumber * secondNumber} ");
+
+                if (operatorChoice == '/')
+                    Console.WriteLine($"Entererd: {firstNumber} {operatorChoice} {secondNumber}. Result: {calculatedResult = firstNumber / secondNumber} ");
+                {
+                    Console.WriteLine(calculatedResult);
+                    calculatedResultsList.Add(calculatedResult);
+                    Console.ReadKey();
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Error: Incorrect input. \nRetry? Y/N?");
+                string? retryCase1 = Console.ReadLine();
+                if (retryCase1 == "y")
+                    goto case 1;
+
+                else if (retryCase1 != "y")
+                            //You will be returned to the meny. WriteLine and then a 2 second delay for break? Possible?
+                            break;
+            }
+            break;
+
+
+
+        case 2:
+            Console.WriteLine("Show the previous results of the calculator? Y/N?");
+            string? showPreviousResults = Console.ReadLine();
+            if (showPreviousResults == "y")
+            //Print list.
+            {
+                Console.WriteLine(calculatedResultsList);
+                Console.ReadKey();
+                break;
+            }
+            
+            else if (showPreviousResults != "y")
+                //You will be returned to the meny. WriteLine and then a 2 second delay for break? Possible?
+                break;
+
+            break;
 
 
 
@@ -40,9 +112,23 @@
 
 
 
+        case 3:
+            Console.WriteLine("If you exit the program the calculations will not be saved. Exit the program? Y/N?");
+            string? exitYesNo = Console.ReadLine();
+
+            if (exitYesNo?.ToLower() == "y")
+                programRunning = false;
+
+            else if (exitYesNo != "y")
+                break;
+            break;
 
 
+        default:
+            Console.WriteLine("You have to chose between meny choices 1 - 3! \nPress a key and then try again.");
+            break;
 
-        }
     }
+    //To clear the console the between meny choices.
+    Console.Clear();
 }
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Basic Calculator
+
+            //Pseudo code in Swedish
+            /*
+            
+            DONE. Välkomnande meddelande
+            
+            -En lista för att spara historik för räkningar
+
+            DONE. Användaren matar in tal och matematiska operation
+
+            -OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+
+            DONE. Ifall användaren skulle dela 0 med 0 visa Ogiltig inmatning!
+
+            -Lägga resultat till listan
+
+            DONE. Visa resultat
+
+            -Fråga användaren om den vill visa tidigare resultat.
+
+            -Visa tidigare resultat
+
+            DONE. Fråga användaren om den vill avsluta eller fortsätta.
+
+            */
